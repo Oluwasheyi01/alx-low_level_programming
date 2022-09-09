2989a269fddc49e19d19efed2af65e933139da94
Oluwasheyi01 <martinsakin7@gmail.com>
2022-09-09 01:22:16 -0700

    lk

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this progran prints *Programing Is like bullding a multilingual puzzle
+ * Retun: 0
+ */
+int main(vold)
+{
+	puts(*\"Programire Is like bullding
+	a multilingual puzzle*):
+	return (0);
+}
