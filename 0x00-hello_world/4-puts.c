361f8527aec61031e6adff6422b5eaada10f0bbe
Oluwasheyi01 <martinsakin7@gmail.com>
2022-09-09 17:32:37 -0700

    asx

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
+
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this progran prints *Programing Is like bullding a multilingual puzzle
- * Retun: 0
- */
-int main(vold)
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
 {
-	puts(*\"Programire Is like bullding
-	a multilingual puzzle*):
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
