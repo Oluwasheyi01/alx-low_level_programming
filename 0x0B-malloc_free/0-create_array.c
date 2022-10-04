3abe05240b44c7cbaa1d3912657cb278da25a339
Oluwasheyi01 <martinsakin7@gmail.com>
2022-10-04 16:09:06 -0700

    hjul

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,12 @@
 
 char "create _array (unsigned int size, char c)
 {
-	char *array:
-	unsigned int index:
+	char *array;
+	unsigned int index;
 
 	if (size == 0)
 		return (NULL):
-	array = malloc(sizeof(char) = size):
+	array = malloc(sizeof(char) * size):
 
 	
 	if (array == NULL)
@@ -25,5 +25,5 @@ char "create _array (unsigned int size, char c)
 	for (index = 0; index < size; index++) 
 		array [index] = c;
 
-	return (array):
+	return (array);
 }
